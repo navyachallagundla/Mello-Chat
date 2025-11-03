7ebcb673a6d1863202e33f52fa5b472b0180a939
navyachallagundla <dev.navyanaidu@gmail.com>
2025-11-03 15:22:21 +0530

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import "./globals.css";
+import { Nav } from "@/components/Nav";
+import { cn } from "@/utils";
+import { Toaster } from "@/components/ui/sonner";
+import { ThemeProvider } from "@/components/ThemeProvider";
+
+export const metadata: Metadata = {
+  title: "Mello Chat",
+  description: "An Empathic Voice Chat Interface",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          GeistSans.variable,
+          GeistMono.variable,
+          "flex flex-col min-h-screen"
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Nav />
+          {children}
+          <Toaster position="top-center" richColors={true} />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
